72b5ce5951ece06e9b3fb6fedfda1c33c1736140
Jambavan <shantanu@olpci.org>
2019-05-28 14:44:31 -0400

    added a new feature in a file

--- Helpers.py
+++ Helpers.py
@@ -13,27 +13,29 @@ class Checks(object):
         if n <= 0:
             raise ValueError("Number of increments must be positive.")
 
+
 def _check_number(self, value, name):
     if not isinstance(value, (int, float)):
         raise TypeError(name + " value must be a number.")
-
+    
     def _check_positive_number(self, value, name):
         self._check_number(value, name)
         if value <= 0:
             raise ValueError(name + " value must be positive.")
 
+
 def _check_nonnegative_number(self, value, name):
     self._check_number(value, name)
-        if value < 0:
-            raise ValueError(name + " value must be nonnegative.")
-                
-                def _check_zero(self, zero):
-                    if not isinstance(zero, bool):
-raise TypeError("Zero inclusion flag must be a boolean.")
+    if value < 0:
+        raise ValueError(name + " value must be nonnegative.")
+            
+            def _check_zero(self, zero):
+                if not isinstance(zero, bool):
+                    raise TypeError("Zero inclusion flag must be a boolean.")
 
 
 class Continuous(Checks):
-    """Base class encompassing the properties for continuous time stochastic processes."
+    """Base class encompassing the properties for continuous stochastic processes."
         """
     
     def __init__(self, t=1):
@@ -56,17 +58,19 @@ class Continuous(Checks):
             self._n = n
             self._times = self.times(n, zero)
 
+
 def _check_time_sequence(self, times):
     increments = np.diff(times)
-        if np.any([t < 0 for t in times]):
-            raise ValueError("Times must be nonnegative.")
+    if np.any([t < 0 for t in times]):
+        raise ValueError("Times must be nonnegative.")
     if np.any([t <= 0 for t in increments]):
         raise ValueError("Times must be strictly increasing.")
         return increments
 
+
 def _linspace(self, end, n, zero=True):
-    """Generate a linspace from 0 to end for n increments."""
-        if zero:
-            return np.linspace(0, end, n + 1)
+"""Generate a field from 0 to end for n increments."""
+    if zero:
+        return np.linspace(0, end, n + 1)
     else:
         return np.linspace(1.0 * end / n, end, n)
